9f193e756bf3e7f497f74b58f1144fb1ecd8eebb
Ziyi Xi <ziyixi@mail.ustc.edu.cn>
2025-09-02 20:58:37 -0700

    Add Notion API environment variables to Dockerfile and entrypoint script

--- todo/entrypoint.sh
+++ todo/entrypoint.sh
@@ -4,4 +4,6 @@
     -port=${PORT} \
     -mailjet-api-key-public=${MAILJET_API_KEY_PUBLIC} \
     -mailjet-api-key-private=${MAILJET_API_KEY_PRIVATE} \
-    -target-email=${TARGET_EMAIL}
+    -target-email=${TARGET_EMAIL} \
+    -notion-api-key=${NOTION_API_KEY} \
+    -notion-database-id=${NOTION_DATABASE_ID} \
